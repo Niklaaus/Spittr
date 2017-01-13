31327fbf4577e65a455ef0ef12604a7ac41fba8b
Kr. Yashwant <kr.yashwant@outlook.com>
2017-01-13 10:47:43 +0530

    Update SpittleController.java

--- src/main/java/com/web/SpittleController.java
+++ src/main/java/com/web/SpittleController.java
@@ -1,25 +1,22 @@
 package com.web;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import com.Spittle;
+
 import com.data.SpittleRepository;
 
 @Controller
 @RequestMapping("/spittles")
 public class SpittleController {
 	private SpittleRepository spittleRepository;
-
 	@Autowired
 	public SpittleController(SpittleRepository spittleRepository) {
 		this.spittleRepository = spittleRepository;
 	}
-
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method=RequestMethod.GET)
 	public String spittles(Model model) {
 		model.addAttribute(spittleRepository.findSpittles(Long.MAX_VALUE, 20));
 		return "spittles";
