ba3a85f3b2d8cd0a65604c9a53e0e8a69e122b8b
Kr. Yashwant <kr.yashwant@outlook.com>
2017-01-13 18:50:13 +0530

    Updated SpittleController.java
    For changes made in paginated jsp

--- src/main/java/com/web/SpittleController.java
+++ src/main/java/com/web/SpittleController.java
@@ -1,24 +1,28 @@
 package com.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.Spittle;
 import com.data.SpittleRepository;
 
 @Controller
 @RequestMapping("/spittles")
 public class SpittleController {
 	private SpittleRepository spittleRepository;
+	private static final String MAX_LONG_AS_STRING = "600000";
 	@Autowired
 	public SpittleController(SpittleRepository spittleRepository) {
 		this.spittleRepository = spittleRepository;
 	}
 	@RequestMapping(method=RequestMethod.GET)
-	public String spittles(Model model) {
-		model.addAttribute(spittleRepository.findSpittles(Long.MAX_VALUE, 20));
-		return "spittles";
+	public List<Spittle> spittles(@RequestParam(value="max", defaultValue=MAX_LONG_AS_STRING) long max,@RequestParam(value="count", defaultValue="20") int count) {
+		return spittleRepository.findSpittles(max, count);
 	}
 }
